309802446067521d37a1fedb7e3425ca60ab2dd3
Prince Narteh <prinarteh@gmail.com>
2021-11-05 05:04:56 +0000

    Add checkForRowOfThree logic function

--- src/App.tsx
+++ src/App.tsx
@@ -36,17 +36,57 @@ const App = () => {
     }
   };
 
+  const checkForRowOfThree = () => {
+    for (let i = 0; i < 64; i++) {
+      const rowOfThree = [i, i + 1, i + 2];
+      const decidedColor = currentColorArrangement[i];
+
+      const notValid = [
+        6, 7, 14, 15, 22, 23, 30, 31, 38, 39, 46, 47, 54, 55, 63, 64,
+      ];
+
+      if (notValid.includes(i)) continue;
+
+      if (
+        rowOfThree.every(
+          (square) => currentColorArrangement[square] === decidedColor
+        )
+      ) {
+        rowOfThree.forEach((square) => (currentColorArrangement[square] = ""));
+      }
+    }
+  };
+
+  const checkForColumnOfFour = () => {
+    for (let i = 0; i < 39; i++) {
+      const columnOfFour = [i, i + width, i + width * 2, i + width * 3];
+      const decidedColor = currentColorArrangement[i];
+
+      if (
+        columnOfFour.every(
+          (square) => currentColorArrangement[square] === decidedColor
+        )
+      ) {
+        columnOfFour.forEach(
+          (square) => (currentColorArrangement[square] = "")
+        );
+      }
+    }
+  };
+
   useEffect(() => {
     createBoard();
   }, []);
 
   useEffect(() => {
     const timer = setInterval(() => {
+      checkForColumnOfFour();
       checkForColumnOfThree();
+      checkForRowOfThree();
       setCurrentColorArrangement([...currentColorArrangement]);
     }, 100);
     return () => clearInterval(timer);
-  }, [checkForColumnOfThree]);
+  }, [checkForColumnOfFour, checkForColumnOfThree, currentColorArrangement]);
 
   console.log(currentColorArrangement);
 
