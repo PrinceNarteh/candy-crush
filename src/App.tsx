b08fe14773839a6eefb81d3c38c6559ff1a5aeb5
Prince Narteh <prinarteh@gmail.com>
2021-11-05 03:48:22 +0000

    Add createBoard function

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const width = 8;
 const candyColors = ["red", "green", "blue", "yellow", "orange", "purple"];
 
 const App = () => {
+  const [currentColorArrangement, setCurrentColorArrangement] = useState<
+    string[]
+  >([]);
+
+  const createBoard = () => {
+    const randomColorArrangement: string[] = [];
+
+    for (let i = 0; i < width * width; i++) {
+      const randomNumber = Math.floor(Math.random() * candyColors.length);
+      const randomColor = candyColors[randomNumber];
+      randomColorArrangement.push(randomColor);
+    }
+    setCurrentColorArrangement(randomColorArrangement);
+  };
+
+  useEffect(() => {
+    createBoard();
+  }, []);
+
   return (
     <div className="App">
       <h1>hello</h1>
