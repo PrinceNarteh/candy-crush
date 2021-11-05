117d17df2ba2e2ec06320194c0de80ffaeb11a9c
Prince Narteh <prinarteh@gmail.com>
2021-11-05 03:28:31 +0000

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const width = 8;
+const candyColors = ["red", "green", "blue", "yellow", "orange", "purple"];
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>hello</h1>
+    </div>
+  );
+};
+
+export default App;
