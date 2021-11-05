2440e4d322298765ee537cf43f326db58e3cb33f
Prince Narteh <prinarteh@gmail.com>
2021-11-05 05:19:56 +0000

    Add checkForRowOfFour logic function

--- src/App.tsx
+++ src/App.tsx
@@ -74,6 +74,27 @@ const App = () => {
     }
   };
 
+  const checkForRowOfFour = () => {
+    for (let i = 0; i < 39; i++) {
+      const rowOfFour = [i, i + 1, i + 2, i + 3];
+      const decidedColor = currentColorArrangement[i];
+      const notValid = [
+        5, 6, 7, 13, 14, 15, 21, 22, 23, 29, 30, 31, 37, 38, 39, 45, 46, 47, 53,
+        54, 55, 61, 62, 63,
+      ];
+
+      if (notValid.includes(1)) continue;
+
+      if (
+        rowOfFour.every(
+          (square) => currentColorArrangement[square] === decidedColor
+        )
+      ) {
+        rowOfFour.forEach((square) => (currentColorArrangement[square] = ""));
+      }
+    }
+  };
+
   useEffect(() => {
     createBoard();
   }, []);
@@ -81,12 +102,19 @@ const App = () => {
   useEffect(() => {
     const timer = setInterval(() => {
       checkForColumnOfFour();
+      checkForRowOfFour();
       checkForColumnOfThree();
       checkForRowOfThree();
       setCurrentColorArrangement([...currentColorArrangement]);
     }, 100);
     return () => clearInterval(timer);
-  }, [checkForColumnOfFour, checkForColumnOfThree, currentColorArrangement]);
+  }, [
+    checkForColumnOfFour,
+    checkForRowOfFour,
+    checkForColumnOfThree,
+    checkForRowOfThree,
+    currentColorArrangement,
+  ]);
 
   console.log(currentColorArrangement);
 
@@ -95,7 +123,11 @@ const App = () => {
       <h1>Candy Crush</h1>
       <div className="game">
         {currentColorArrangement.map((candyColor, index) => (
-          <img key={index} style={{ backgroundColor: candyColor }} />
+          <img
+            key={index}
+            style={{ backgroundColor: candyColor }}
+            alt={`${index}`}
+          />
         ))}
       </div>
     </div>
