9cbe6d64abdce4bc208411844cef08ce443f6db2
Prince Narteh <prinarteh@gmail.com>
2021-11-05 04:38:33 +0000

    Add checkForColumnOfThree logic function

--- src/App.tsx
+++ src/App.tsx
@@ -19,9 +19,34 @@ const App = () => {
     setCurrentColorArrangement(randomColorArrangement);
   };
 
+  const checkForColumnOfThree = () => {
+    for (let i = 0; i < 47; i++) {
+      const columnOfThree = [i, i + width, i + width * 2];
+      const decidedColor = currentColorArrangement[i];
+
+      if (
+        columnOfThree.every(
+          (square) => currentColorArrangement[square] === decidedColor
+        )
+      ) {
+        columnOfThree.forEach(
+          (square) => (currentColorArrangement[square] = "")
+        );
+      }
+    }
+  };
+
   useEffect(() => {
     createBoard();
   }, []);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      checkForColumnOfThree();
+      setCurrentColorArrangement([...currentColorArrangement]);
+    }, 100);
+    return () => clearInterval(timer);
+  }, [checkForColumnOfThree]);
 
   console.log(currentColorArrangement);
 
